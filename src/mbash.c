94d63cbaf3d860c17f64937b26e4a5c0b121938e
Packingdustry <alois.mc@hotmail.com>
2025-01-14 16:34:37 +0100

    ajoute un fichier listant les fonctionnalités (vide pour l'instant)

--- src/mbash.c
+++ src/mbash.c
@@ -4,10 +4,12 @@
 #include <unistd.h>
 #include <assert.h>
 #define MAXLI 2048
+
 char cmd[MAXLI];
 char path[MAXLI];
 int pathidx;
 void mbash();
+
 int main(int argc, char** argv) {
   while (1) {
     printf("Commande: ");
